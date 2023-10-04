d7dda3e6ab91f28273f4120b3e243fbd3f0a2e88
Akmalkhon Burkhonov <akmalburhonov5515@gmail.com>
2023-10-04 09:19:49 +0500

    Initial Commit

---
+++ Otaku V2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Otaku V2
+//
+//  Created by The WORLD on 04.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
